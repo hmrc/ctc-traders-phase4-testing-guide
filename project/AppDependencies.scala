c9b76968e1cc330d7407ab6f5cedef613621f010
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2023-11-22 09:19:38 +0000

    CTCP-4448: Update dependencies, remove public files as they are generated anyway

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,16 +1,14 @@
-import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "7.13.0"
+  val bootStrapPlayVersion = "7.23.0"
 
   val compile: Seq[ModuleID] = Seq(
-    ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.9" % "test",
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test",
+    "org.scalatest" %% "scalatest" % "3.2.11" % "test",
+    "com.vladsch.flexmark" % "flexmark-all" % "0.64.0" % "test",
     "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootStrapPlayVersion % "test"
   )
 }
